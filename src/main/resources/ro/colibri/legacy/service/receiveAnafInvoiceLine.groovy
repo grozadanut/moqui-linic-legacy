220d918f063fa8f06f619498c3a55601f1b8dfa1
Groza Danut <groza_danut@yahoo.com>
2025-10-24 13:54:39 +0300

    Receive ANAF Invoice Line

---
+++ src/main/resources/ro/colibri/legacy/service/receiveAnafInvoiceLine.groovy
@@ -0,0 +1,50 @@
+package ro.colibri.legacy.service
+
+import org.moqui.context.ExecutionContext
+
+ExecutionContext ec = context.ec
+
+lineMsg = ec.getEntity().find("moqui.service.message.SystemMessage")
+        .condition("systemMessageId", systemMessageId)
+        .one()
+
+if (lineMsg.statusId != 'SmsgConsumed') {
+    ec.message.addError(ec.resource.expand('''System message [${systemMessageId}] has status [${lineMsg.statusId}] and must be SmsgConsumed, not receiving.''' ?: "Error in actions",''))
+    return
+}
+
+if (lineMsg.systemMessageTypeId != 'ANAFReceivedInvoiceLine') {
+    ec.message.addError(ec.resource.expand('''System message [${systemMessageId}] has message type [${lineMsg.systemMessageTypeId}] and must be ANAFReceivedInvoiceLine, not receiving.''' ?: "Error in actions",''))
+    return
+}
+
+// Sistemul actualizeaza codul furnizorului pentru produsul respectiv
+if (supplierId != null && productId != null && facilityPartyId != null &&
+        supplierProductId != null) {
+    ec.service.sync().name("store", "mantle.product.ProductPrice")
+            .parameters([productPriceId: supplierId+"_"+productId+"_"+facilityPartyId,
+                         productId: productId,
+                         vendorPartyId: supplierId,
+                         customerPartyId: facilityPartyId,
+                         "otherPartyItemId": supplierProductId,
+                         "otherPartyItemName": supplierProductName])
+            .call()
+}
+
+// marcheaza linia de pe eFactura ca receptionata
+ec.service.sync().name("update#moqui.service.message.SystemMessage")
+        .parameters([systemMessageId:systemMessageId, statusId:'SmsgConfirmed', processedDate:nowDate])
+        .call()
+
+// Dupa ce ultima linie a fost receptionata, Sistemul marcheaza eFactura ca receptionata.
+var allLinesNo = ec.getEntity().find("moqui.service.message.SystemMessage")
+        .condition("parentMessageId", lineMsg.parentMessageId)
+        .count()
+var receivedLinesNo = ec.getEntity().find("moqui.service.message.SystemMessage")
+        .condition("parentMessageId", lineMsg.parentMessageId)
+        .condition("statusId", "SmsgConfirmed")
+        .count()
+if (allLinesNo == receivedLinesNo)
+    ec.service.async().name("AnafServices.receive#AnafInvoice")
+            .parameters([systemMessageId:lineMsg.parentMessageId, invoiceId:invoiceId])
+            .call()
--- src/test/groovy/AnafServicesTests.groovy
+++ src/test/groovy/AnafServicesTests.groovy
@@ -40,6 +40,24 @@ class AnafServicesTests extends Specification {
         ec.service.sync().name("delete#moqui.service.message.SystemMessage")
                 .parameters([systemMessageId: "AST_4"])
                 .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "AST_1_1"])
+                .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "AST_2_1"])
+                .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "AST_3_1"])
+                .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "AST_4_1"])
+                .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "ASTL_5_1"])
+                .call()
+        ec.service.sync().name("delete#moqui.service.message.SystemMessage")
+                .parameters([systemMessageId: "ASTL_5"])
+                .call()
 
         ec.artifactExecution.enableAuthz()
     }
@@ -129,5 +147,132 @@ class AnafServicesTests extends Specification {
                 .condition("systemMessageId", "AST_4")
                 .condition("invoiceId", "AST_INV_1")
                 .one() != null
+    }
+
+    def "receive#AnafInvoiceLine error when statusId not SmsgConsumed"() {
+        setup:
+        EntityValue msg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        msg.set("systemMessageId", "AST_1_1")
+        msg.set("statusId", "SmsgReceived")
+        msg.set("systemMessageTypeId", "ANAFReceivedInvoiceLine")
+        msg.set("isOutgoing", "N")
+        msg.createOrUpdate()
+
+        when:
+        ec.service.sync().name("AnafServices.receive#AnafInvoiceLine")
+                .parameters([systemMessageId: "AST_1_1"])
+                .call()
+
+        then:
+        ec.message.getErrorsString() == "System message [AST_1_1] has status [SmsgReceived] and must be SmsgConsumed, not receiving.\n"
+        ec.message.clearErrors()
+    }
+
+    def "receive#AnafInvoiceLine error when systemMessageTypeId not ANAFReceivedInvoiceLine"() {
+        setup:
+        EntityValue msg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        msg.set("systemMessageId", "AST_2_1")
+        msg.set("statusId", "SmsgConsumed")
+        msg.set("systemMessageTypeId", "ANAFReceivedInvoice")
+        msg.set("isOutgoing", "N")
+        msg.createOrUpdate()
+
+        when:
+        ec.service.sync().name("AnafServices.receive#AnafInvoiceLine")
+                .parameters([systemMessageId: "AST_2_1"])
+                .call()
+
+        then:
+        ec.message.getErrorsString() == "System message [AST_2_1] has message type [ANAFReceivedInvoice] and must be ANAFReceivedInvoiceLine, not receiving.\n"
+        ec.message.clearErrors()
+    }
+
+    def "receive#AnafInvoiceLine status to SmsgConfirmed when success"() {
+        setup:
+        EntityValue msg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        msg.set("systemMessageId", "AST_3_1")
+        msg.set("statusId", "SmsgConsumed")
+        msg.set("systemMessageTypeId", "ANAFReceivedInvoiceLine")
+        msg.set("isOutgoing", "N")
+        msg.createOrUpdate()
+
+        when:
+        ec.service.sync().name("AnafServices.receive#AnafInvoiceLine")
+                .parameters([systemMessageId: "AST_3_1"])
+                .call()
+
+        then:
+        ec.message.errors.size() == 0
+        ec.getEntity().find("moqui.service.message.SystemMessage")
+                .condition("systemMessageId", "AST_3_1")
+                .one()
+                .get("statusId") == "SmsgConfirmed"
+    }
+
+    def "receive#AnafInvoiceLine update product supplier id"() {
+        setup:
+        EntityValue msg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        msg.set("systemMessageId", "AST_4_1")
+        msg.set("statusId", "SmsgConsumed")
+        msg.set("systemMessageTypeId", "ANAFReceivedInvoiceLine")
+        msg.set("isOutgoing", "N")
+        msg.createOrUpdate()
+
+        when:
+        ec.service.sync().name("AnafServices.receive#AnafInvoiceLine")
+                .parameters([systemMessageId: "AST_4_1"])
+                .parameters([supplierId: "supplier1"])
+                .parameters([productId: "59"])
+                .parameters([facilityPartyId: "L2"])
+                .parameters([supplierProductId: "HOL59320"])
+                .parameters([supplierProductName: "HOLCIM CIMENT 40KG"])
+                .call()
+
+        then:
+        ec.message.errors.size() == 0
+        ec.getEntity().find("moqui.service.message.SystemMessage")
+                .condition("systemMessageId", "AST_4_1")
+                .one()
+                .get("statusId") == "SmsgConfirmed"
+        var price = ec.getEntity().find("mantle.product.ProductPrice")
+                .condition("productPriceId", "supplier1_59_L2")
+                .one()
+        price != null
+        price.get("otherPartyItemId") == "HOL59320"
+        price.get("otherPartyItemName") == "HOLCIM CIMENT 40KG"
+    }
+
+    def "receive#AnafInvoiceLine mark invoice as received if last line"() {
+        setup:
+        EntityValue msg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        msg.set("systemMessageId", "ASTL_5")
+        msg.set("statusId", "SmsgConsumed")
+        msg.set("systemMessageTypeId", "ANAFReceivedInvoice")
+        msg.set("isOutgoing", "N")
+        msg.createOrUpdate()
+
+        EntityValue lineMsg = ec.getEntity().makeValue("moqui.service.message.SystemMessage")
+        lineMsg.set("systemMessageId", "ASTL_5_1")
+        lineMsg.set("statusId", "SmsgConsumed")
+        lineMsg.set("systemMessageTypeId", "ANAFReceivedInvoiceLine")
+        lineMsg.set("isOutgoing", "N")
+        lineMsg.set("parentMessageId", "ASTL_5")
+        lineMsg.createOrUpdate()
+
+        when:
+        ec.service.sync().name("AnafServices.receive#AnafInvoiceLine")
+                .parameters([systemMessageId: "ASTL_5_1"])
+                .call()
+
+        then:
+        ec.message.errors.size() == 0
+        ec.getEntity().find("moqui.service.message.SystemMessage")
+                .condition("systemMessageId", "ASTL_5_1")
+                .one()
+                .get("statusId") == "SmsgConfirmed"
+        ec.getEntity().find("moqui.service.message.SystemMessage")
+                .condition("systemMessageId", "ASTL_5")
+                .one()
+                .get("statusId") == "SmsgConfirmed"
     }
 }
